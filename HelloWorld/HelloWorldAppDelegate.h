3556b5c7b5fbc38f0e28d4b7de13f04fa33716d9
Simon Garcia <simon.garciar@gmail.com>
2013-11-01 19:25:39 +0100

    Initial Commit

---
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldAppDelegate.h
+//  HelloWorld
+//
+//  Created by CamonZ on 11/1/13.
+//  Copyright (c) 2013 CamonZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
