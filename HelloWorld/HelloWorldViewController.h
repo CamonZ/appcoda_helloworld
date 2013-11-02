b9713b4c70b878978d29588bffefd9fc2b2e35ce
Simon Garcia <simon.garciar@gmail.com>
2013-11-02 20:02:07 +0100

    Finish HelloWorld Tutorial

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,15 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by CamonZ on 11/1/13.
+//  Copyright (c) 2013 CamonZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+-(IBAction)showMessage;
+
+@end
